5f686c70b92852a2a7d5f8b37d70d35a563fd029
altom <altomhussian2001@yahoo.com>
2022-07-18 21:14:33 +0100

    On my new branch

--- factorial.js
+++ factorial.js
@@ -1,24 +1,22 @@
 //1. Normal way to find factorial of a number
 function factorial(num) {
-    let fact = 1;
-    for (let i = num; i > 0; i--){
-        fact *= i;
-    }
-    console.log(fact);
+  let fact = 1;
+  for (let i = num; i > 0; i--) {
+    fact *= i;
+  }
+  console.log(fact);
 }
 factorial(5);
 
-
 function factorialRecursion(num) {
-    if(num==0 || num==1) {
-        console.log(num);
-        return num;
-    };
-    return num * factorialRecursion(num - 1);
-
+  if (num == 0 || num == 1) {
+    console.log(num);
+    return num;
+  }
+  return num * factorialRecursion(num - 1);
 }
 factorialRecursion(5);
 
-
-
 ///This is my factorial algorithem
+
+//Another line on testing
